38f3c37d2677a4779bc7c0e4827b67214263ba5f
Ilma Ainur Rohma <ilma13rohma@gmail.com>
2020-08-09 21:32:19 +0700

    [QUIZ 3] make route

--- routes/web.php
+++ routes/web.php
@@ -12,16 +12,21 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('erd');
 });
 Route::get('/master', function () {
     return view('layouts.master');
 });
 
-Route::get('/items/create', 'ItemController@create'); // menampilkan halaman form
-Route::post('/items', 'ItemController@store'); // menyimpan data
-Route::get('/items', 'ItemController@index'); // menampilkan semua
-Route::get('/items/{id}', 'ItemController@show'); // menampilkan detail item dengan id 
-Route::get('/items/{id}/edit', 'ItemController@edit'); // menampilkan form untuk edit item
-Route::put('/items/{id}', 'ItemController@update'); // menyimpan perubahan dari form edit
-Route::delete('/items/{id}', 'ItemController@destroy'); // menghapus data dengan id
+// Route::get('/items/create', 'ItemController@create'); // menampilkan halaman form
+// Route::post('/items', 'ItemController@store'); // menyimpan data
+// Route::get('/items', 'ItemController@index'); // menampilkan semua
+// Route::get('/items/{id}', 'ItemController@show'); // menampilkan detail item dengan id 
+// Route::get('/items/{id}/edit', 'ItemController@edit'); // menampilkan form untuk edit item
+// Route::put('/items/{id}', 'ItemController@update'); // menyimpan perubahan dari form edit
+// Route::delete('/items/{id}', 'ItemController@destroy'); // menghapus data dengan id
+
+Route::resource('proyek', 'ProjectsController'); //resource akan otomatis mengenerate 
+
+Route::get('users/{id}/daftarkan-staff', 'ProjectsController');
+Route::post('users/{id}/daftarkan-staff', 'ProjectsController');
